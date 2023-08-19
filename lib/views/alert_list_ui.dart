c9c0394c0d9eb89fdcb3714fcbade7cc19101aac
thanakrit <ketaauto55@gmail.com>
2023-08-19 18:14:11 +0700

    Add DatePicker

--- lib/views/alert_list_ui.dart
+++ lib/views/alert_list_ui.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_local_variable, deprecated_member_use
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:snake_guard/views/alert_description_ui.dart';
+import 'package:intl/intl.dart';
+// import 'package:snake_guard/views/alert_description_ui.dart';
 
 class AlertListUI extends StatefulWidget {
   const AlertListUI({super.key});
@@ -11,6 +12,8 @@ class AlertListUI extends StatefulWidget {
 }
 
 class _AlertListUIState extends State<AlertListUI> {
+  DateTime date = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,13 +23,69 @@ class _AlertListUIState extends State<AlertListUI> {
           child: Column(
             children: [
               SizedBox(
-                height: MediaQuery.of(context).size.width * 0.2,
+                height: MediaQuery.of(context).size.width * 0.15,
               ),
-              Text(
-                'Notificaion',
-                style: GoogleFonts.itim(
-                  color: Colors.white,
-                  fontSize: MediaQuery.of(context).size.width * 0.1,
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18), // <-- Radius
+                  ),
+                  backgroundColor: Colors.black,
+                ),
+                onPressed: () async {
+                  DateTime? newDate = await showDatePicker(
+                    context: context,
+                    initialDate: date,
+                    firstDate: DateTime(2023),
+                    lastDate: DateTime.now(),
+                    builder: (context, child) => Theme(
+                      data: ThemeData().copyWith(
+                        textTheme: TextTheme(
+                          headline1: GoogleFonts.itim(), // Selected Date
+                          headline2: GoogleFonts.itim(),
+                          headline3: GoogleFonts.itim(),
+                          headline4: GoogleFonts.itim(),
+                          headline5: GoogleFonts.itim(),
+                          headline6: GoogleFonts.itim(),
+                          overline: GoogleFonts.itim(), // Title - SELECT DATE
+                          bodyText1: GoogleFonts.itim(), // year gridbview picker
+                          subtitle1: GoogleFonts.itim(color: Colors.white), // input
+                          subtitle2: GoogleFonts.itim(), // month/year picker
+                          caption: GoogleFonts.itim(), // days
+                        ),
+                        colorScheme: ColorScheme.dark(
+                          primary: Color.fromRGBO(49, 66, 71, 1), // header background color
+                          onPrimary: Colors.white, // select day color
+                          onSurface: Colors.white, // body text color
+                        ),
+                        dialogBackgroundColor: Colors.black,
+                        textButtonTheme: TextButtonThemeData(
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.white, // button text color
+                            textStyle: GoogleFonts.itim(),
+                          ),
+                        ),
+                        inputDecorationTheme: InputDecorationTheme(
+                          labelStyle: GoogleFonts.itim(), // Input label
+                        ),
+                      ),
+                      child: child!,
+                    ),
+                  );
+
+                  //if 'CANCEL' => null
+                  if (newDate == null) return;
+
+                  //if 'OK' => DateTime
+                  setState(() => date = newDate);
+                },
+                icon: Icon(Icons.calendar_today),
+                label: Text(
+                  '${date.day}/${date.month}/${date.year}',
+                  style: GoogleFonts.itim(
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.width * 0.06,
+                  ),
                 ),
               ),
             ],
